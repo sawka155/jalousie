f194a382edb1c17727857cc0bdb161a11bdc8008
Sawka <vasilev9000@mail.ru>
2020-11-24 18:37:25 +0300

    Header_full

--- js/script.js
+++ js/script.js
@@ -1,24 +1,62 @@
 const nav_button = $(".nav_block");
-
-$(nav_button).hover(
-    function () {
-        $(this).css({
-            'background': '#2A2A2A',
-            'opacity': '1',
-            'visibility': 'visible',
-            'transition': '1s',
-            'border': '0.1px solid #DDDDDD',
-            'box-sizing': 'border-box',
-
-        })
-    },
-    function () {
-        $(this).css({
-            'background': '',
-            'opacity': '1',
-            'visibility': '',
-            'transition': '1s',
-            'border': '0.1px solid #333333',
-            'box-sizing': ''
-        })
+const header_right_button = $(".header_top__right");
+const header_mobile__toggle = $(".header_mobile__toggle img");
+const header_mobile__open = $('.header_mobile__open');
+$(nav_button).hover(function () {
+    $(this).css({
+        'background': '#2A2A2A',
+        'opacity': '1',
+        'visibility': 'visible',
+        'transition': '0.5s',
+        'border': '0.1px solid #DDDDDD',
+        'box-sizing': 'border-box',
     })
+}, function () {
+    $(this).css({
+        'background': '',
+        'opacity': '1',
+        'visibility': '',
+        'border': '0.1px solid #333333',
+        'box-sizing': ''
+    })
+})
+
+$(nav_button).mousedown(function (e) {
+    if (e.detail > 1) {
+        e.preventDefault();
+    }
+});
+$(header_right_button).mousedown(function (e) {
+    if (e.detail > 1) {
+        e.preventDefault();
+    }
+});
+
+
+
+
+$(header_mobile__toggle).click(function (e) {
+    var close = $('.header_mobile__open').css('opacity');
+    if (close == '1') {
+        $('.header_mobile__open').css({
+            'opacity': '',
+            'transition': '1s',
+            'visibility': ''
+        })
+    } else {
+        $('.header_mobile__open').css({
+            'opacity': '1',
+            'transition': '0.5s',
+            'visibility': 'visible'
+        })
+    }
+    $("*").mousedown(function (e) {
+        if ($(".header_mobile__open").has(e.target).length === 0) {
+            $(".header_mobile__open").css({
+                'opacity': '',
+                'transition': '1s',
+                'visibility': ''
+            });
+        }
+    });
+});
